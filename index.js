af8295c68657c3cafd5fa1f473f5fe9cf327a80e
JAYA jaya <js692425@gmail.com>
2023-04-29 14:51:08 +0530

    adding all files

---
+++ index.js
@@ -0,0 +1,51 @@
+var x=document.querySelectorAll("button").length;
+for(let i=0;i<x;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        var ch=this.innerHTML;
+        makeSound(ch);
+        buttonAnimation(ch);
+    })
+}
+
+document.addEventListener("keydown",function(event){
+       makeSound(event.key);
+       buttonAnimation(event.key);
+});
+
+
+function makeSound(key)
+{
+       switch (key) {
+              case "w":var crash=new Audio("sounds\\crash.mp3");
+                     crash.play();
+                     break;
+              case "a":var kick=new Audio("sounds\\kick-bass.mp3");
+                     kick.play();
+                     break;
+              case "s":var snare=new Audio("sounds\\snare.mp3");
+                     snare.play();
+                     break;
+              case "d":var tom1=new Audio("sounds\\tom-1.mp3");
+                     tom1.play();
+                     break;
+              case "j":var tom2=new Audio("sounds\\tom-2.mp3");
+                     tom2.play();
+                     break;
+              case "k":var tom3=new Audio("sounds\\tom-3.mp3");
+                     tom3.play();
+                     break;
+              case "l":var tom4=new Audio("sounds\\tom-4.mp3");
+                     tom4.play();
+                     break;
+              default:console.log(ch);
+                  break;
+          }
+}
+
+function buttonAnimation(currentKey){
+       var activeButton=document.querySelector("."+currentKey);
+activeButton.classList.add("pressed");
+setTimeout(function(){
+    activeButton.classList.remove("pressed")   
+},200);
+}
